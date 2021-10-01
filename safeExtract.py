0184fa22a90d40f6561c4eb59efc3e6f0b9a497a
t-bonds <tjbonds98@gmail.com>
2021-09-30 19:43:10 -0500

    Overdue Commit Cleanup

--- safeExtract.py
+++ safeExtract.py
@@ -7,14 +7,14 @@ import sys
 fileName = sys.argv[1]
 s = SafeBrowsing("AIzaSyC5zRGDds2v6--ZlDDyM274sv1Ueg4jC9M")
 
-f = open(fileName, "r", encoding = 'utf-8', errors = 'ignore')
+f = open(fileName, "r", encoding='utf-8', errors='ignore')
 lines = f.readlines()
 for line in lines:
     link = re.findall(r'(https?://[^\s]+)', line)
     r = s.lookup_urls(link)
     print(str(r))
-    w = open("safelink.txt", "a+", encoding = 'utf-8', errors = 'ignore')
-    w.write(str(r)+"\n")
+    w = open("safelink.txt", "a+", encoding='utf-8', errors='ignore')
+    w.write(str(r) + "\n")
     w.close()
     line.strip()
 f.close()
